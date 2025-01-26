ea5a8e8a857a1dd670db7618e3c5bfc90e63a3c8
Aamal Hussain <aamal.a.hussain@gmail.com>
2025-01-26 22:00:50 +0000

    add verbose to QL steps and change default values for sato

--- QL.py
+++ QL.py
@@ -1,32 +1,40 @@
 import numpy as np
 from networkgames import get_payoffs
+from tqdm import trange
+
 
 def check_var(traj, window_size, tol):
     return np.mean(np.var(traj[:, -window_size:], axis=1)) <= tol
 
 
 def check_rel_diff(traj, window_size, tol):
-    rel_diff = np.max(traj[:, -window_size:], axis=1) - np.min(traj[:, -window_size:], axis=1)
+    rel_diff = np.max(traj[:, -window_size:], axis=1) - np.min(
+        traj[:, -window_size:], axis=1
+    )
     rel_diff /= np.max(traj[:, -window_size:], axis=1)
 
     return np.all(rel_diff <= tol)
 
+
 def init_strats(n_agents, n_actions):
     return np.random.dirichlet(np.ones(n_actions), size=n_agents)
+
 
 def ql_step(P, Q, T, alpha):
     Q = (1 - alpha) * Q + alpha * P
     x = np.exp(Q / T) / np.sum(np.exp(Q / T), axis=1)[:, np.newaxis]
     return x, Q
 
-def run_ql(x, Q, n_iter, edgeset, G, n_agents, n_actions, T):
 
+def run_ql(x, Q, n_iter, edgeset, G, n_agents, n_actions, T, verbose=False):
     alpha = 0.1
     allx = np.zeros((n_iter, n_agents * n_actions))
 
-    for c_iter in range(n_iter):
+    iterator = trange(n_iter) if verbose else range(n_iter)
+
+    for c_iter in iterator:
         P = get_payoffs(x, edgeset, G, n_agents, n_actions)
         x, Q = ql_step(P, Q, T, alpha)
-        allx[c_iter] = x.reshape((n_agents*n_actions))
+        allx[c_iter] = x.reshape((n_agents * n_actions))
 
     return x, Q, allx
--- networkgames.py
+++ networkgames.py
@@ -25,9 +25,9 @@ def shapley(beta=0.2, n_edges=3):
     return A, B
 
 
-def sato(ex=0.1, ey=-0.05, n_edges=3):
-    A = np.dstack([np.array([[ex, -1, 1], [1, ex, -1], [-1, 1, ex]])] * n_edges)
-    B = np.dstack([np.array([[ey, -1, 1], [1, ey, -1], [-1, 1, ey]])] * n_edges)
+def sato(ex=0.5, ey=-0.3, n_edges=3):
+    A = np.dstack([np.array([[ex, 1, -1], [-1, ex, 1], [1, -1, ex]])] * n_edges)
+    B = np.dstack([np.array([[ey, 1, -1], [-1, ey, 1], [1, -1, ey]])] * n_edges)
 
     return A, B
 
@@ -60,8 +60,8 @@ def get_payoffs(x, edgeset, G, n_agents, n_actions):
     P = np.zeros((n_agents, n_actions))
     A, B = G
     for cI, e in enumerate(edgeset):
-        P[e[0]] += np.einsum("ij,j->i", A[:, :, cI], x[e[1]])
-        P[e[1]] += np.einsum("ij,j->i", B[:, :, cI], x[e[0]])
+        P[e[0]] += A[:, :, cI] @ x[e[1]]
+        P[e[1]] += B[:, :, cI] @ x[e[0]]
 
     return P
 
